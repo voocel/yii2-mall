21ca0f77f5fb5aa98d430eba62b4c0fd58fdefa5
voocel <voocel@gmail.com>
2017-12-03 23:40:54 +0800

    init

---
+++ controllers/OrderController.php
@@ -0,0 +1,16 @@
+<?php
+namespace app\controllers;
+use yii\web\Controller;
+
+class OrderController extends Controller{
+
+    public function actionIndex(){
+        $this->layout='layout2';
+        return $this->render('index');
+    }
+
+    public function actionCheck(){
+        $this->layout='layout1';
+        return $this->render('check');
+    }
+}
---
+++ controllers/ProductController.php
@@ -0,0 +1,16 @@
+<?php
+namespace app\controllers;
+use yii\web\Controller;
+
+class ProductController extends Controller{
+
+    public function actionIndex(){
+        $this->layout='layout2';
+        return $this->render('index');
+    }
+
+    public function actionDetail(){
+        $this->layout='layout2';
+        return $this->render('detail');
+    }
+}
